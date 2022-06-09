e85a5ec67a3e2f834c0e99d220b3166248e58d4b
anisharai10 <anisharai102002@gmail.com>
2022-06-09 13:11:51 +0530

    Updates

---
+++ precache-manifest.85910426aa4836ae4e624c8800b7cc3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975bc10aa5cbb4dfeec48fbd5cbe4d85",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "71957d868cfcec074cf7",
+    "url": "/login/static/css/main.d6bc27a5.chunk.css"
+  },
+  {
+    "revision": "61d5a95b69565ddd82c8",
+    "url": "/login/static/js/2.b1705995.chunk.js"
+  },
+  {
+    "revision": "71957d868cfcec074cf7",
+    "url": "/login/static/js/main.c428e5e5.chunk.js"
+  },
+  {
+    "revision": "d036c0eadd0ee639ddc1",
+    "url": "/login/static/js/runtime~main.299f3dfc.js"
+  },
+  {
+    "revision": "c23c1d049bf1facbfa63ac8a8b1c1e06",
+    "url": "/login/static/media/space.c23c1d04.png"
+  }
+]);
